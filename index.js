fc72249143166c32e1c1b7b62407869df355666f
shafiul-osd <mdshafiulislamsafi2506@gmail.com>
2024-06-03 14:41:24 +0600

    Api created for Checking Admin

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const { MongoClient, ServerApiVersion } = require("mongodb");
-require('dotenv').config()
+require("dotenv").config();
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -10,8 +10,7 @@ const port = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 console.log(process.env.DB_PASSWORD);
-const uri =
-  `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.wugjgdu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.wugjgdu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -28,7 +27,6 @@ const userCollection = database.collection("userDB");
 
 async function run() {
   try {
-
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
@@ -38,18 +36,31 @@ async function run() {
     });
 
     // save new user to database
-    app.post("/user", async(req,res)=>{
-        const userInfo = req.body;
-        const query = {email : userInfo.email}
-        const exist = await userCollection.findOne(query);
-        if(exist){
-            return res.send({exist : true})
-        }
-        console.log(exist);
-        const result = await userCollection.insertOne(userInfo)
-        res.send({exist : false})
-    })
+    app.post("/user", async (req, res) => {
+      const userInfo = req.body;
+      const query = { email: userInfo.email };
+      const exist = await userCollection.findOne(query);
+      if (exist) {
+        return res.send({ exist: true });
+      }
+      console.log(exist);
+      const result = await userCollection.insertOne(userInfo);
+      res.send({ exist: false });
+    });
 
+
+    // check is Admin
+    app.get("/user/admin/:email" , async (req,res)=>{
+        const email = req.params.email;
+        const query = {email : email};
+
+        let admin = false;
+        const user = await userCollection.findOne(query)
+        if(user){
+            admin = user?.role === "admin"
+        }
+        res.send({admin})
+    })
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
